112a3fa215e1dff0506fd9df4072c8226af4b4f2
Anas <com.anas.idms.appleid.prd.001784-08-5d674918-865b-4553-9bf0-c2326b981e34@AB-Macbook.local>
2019-08-29 11:18:27 -0700

    Added README.md file

---
+++ README.md
@@ -0,0 +1,9 @@
+Mad Libs is a phrasal template word game where one player prompts others
+for a list of words to substitute for blanks in a story, before reading the
+ often comical or nonsensical story aloud.
+
+Instructions
+1. Open terminal
+2. run -> python3 madlibs.py
+3. Enter prompted information
+4. Automatically prints output to terminal
--- madlibs.py
+++ madlibs.py
@@ -2,13 +2,19 @@
 # an _______________ athlete, and Carl Ludwing Long, jumper from _____________,
 # were competing up to their celebration. This particular scene stood out to
 #  all of us because it was one of the most ________ scene in the movie.
-
+words = list()
 # 1. Adjective
 # 2. Actor/Actress
 # 3. Race
 # 4. Country
 # 5. Verb
 
+# CREATE
+def create(word):
+    words.append(word)
+
+def read(index):
+    return words[index]
 
 def user_input(prompt):
     # the input function will display a message in the terminal
@@ -16,18 +22,19 @@ def user_input(prompt):
     user_input = input(prompt)
     return user_input
 
-
+def print_to_screen():
+        print("One of the "+ read(0) +" and most gripping scenes of the movie was when " + read(1) +
+        ",an " + read(2) + " athlete, and Carl Ludwing Long, jumper from " + read(3) +
+        ",were competing up to their celebration. This particular scene stood out to "+
+         "all of us because it was one of the most " + read(4) +  " scene in the movie.")
 
 def test():
-    adjective = user_input("Enter an adjective: ")
-    actor = user_input("Enter an actor/actress: ")
-    race = user_input("Enter an race: ")
-    country = user_input("Enter an country: ")
-    verb = user_input("Enter an verb: ")
+    create(user_input("Enter an adjective: "))
+    create(user_input("Enter an actor/actress: "))
+    create(user_input("Enter an race: "))
+    create(user_input("Enter an country: "))
+    create(user_input("Enter an verb: "))
 
-    print("One of the "+ adjective +" and most gripping scenes of the movie was when " + actor +
-    ",an " + race + " athlete, and Carl Ludwing Long, jumper from " + country +
-    ",were competing up to their celebration. This particular scene stood out to "+
-     "all of us because it was one of the most" + verb +  "scene in the movie.")
+    print_to_screen()
 
 test()
