6438c85c9a638ab2b66df2188397e62590483034
Anas <com.anas.idms.appleid.prd.001784-08-5d674918-865b-4553-9bf0-c2326b981e34@AB-Macbook.local>
2019-09-03 19:16:35 -0700

    organized code and updated README.md

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
-Mad Libs is a phrasal template word game where one player prompts others
-for a list of words to substitute for blanks in a story, before reading the
- often comical or nonsensical story aloud.
+Author: Anas Bellouzi
+Date: 09/03/2019
 
 Instructions
 1. Open terminal
 2. run -> python3 madlibs.py
 3. Enter prompted information
 4. Automatically prints output to terminal
+5. If user leaves input blank, program fills in blank
+
+One of the ________ and most gripping scenes of the movie was when ____________,
+an _______________ athlete, and Carl Ludwing Long, jumper from _____________,
+were competing up to their celebration. This particular scene stood out to
+all of us because it was one of the most ________ scene in the movie.
+
+1. Adjective
+2. Actor/Actress
+3. Race
+4. Country
+5. Verb
--- madlibs.py
+++ madlibs.py
@@ -2,38 +2,37 @@
 # an _______________ athlete, and Carl Ludwing Long, jumper from _____________,
 # were competing up to their celebration. This particular scene stood out to
 #  all of us because it was one of the most ________ scene in the movie.
-words = list()
 # 1. Adjective
 # 2. Actor/Actress
 # 3. Race
 # 4. Country
 # 5. Verb
 
+words = list()
+
 # CREATE
 def create(word):
     words.append(word)
 
+# READ
 def read(index):
     return words[index]
 
+# USER INPUT
 def user_input(prompt):
     # the input function will display a message in the terminal
     # and wait for user input.
     user_input = input(prompt)
     return user_input
 
-
-
+# PRINT TO SCREEN
 def print_to_screen():
         print("One of the \033[0;32m"+ read(0) +"\033[0m and most gripping scenes of the movie was when \033[33;0;33m" + read(1) +
         "\033[0m,an \033[35;5;35m" + read(2) + "\033[0m athlete, and Carl Ludwing Long, jumper from \033[37;4;37m" + read(3) +
         "\033[0m,were competing up to their celebration. This particular scene stood out to "+
          "all of us because it was one of the most \033[36;6;36m" + read(4) +  "\033[0m scene in the movie.")
 
-# print("\033[0;32mGREEN TEXT\033[0m")
-# print "\033[5;41;32mGREEN TEXT\033[0m"
-
-
+# TEST PROGRAM
 def test():
     adjective = user_input("Enter an adjective: ")
     actor = user_input("Enter an actor/actress: ")
